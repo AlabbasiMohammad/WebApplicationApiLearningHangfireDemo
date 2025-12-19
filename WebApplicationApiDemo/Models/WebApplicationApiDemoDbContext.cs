8f69b6b4ee8e0934502e81474df9968c20a69144
Mohammed Al-Abbasi <mohammed.al-abbasi@atwork.at>
2025-12-19 15:42:50 +0100

    test

--- WebApplicationApiDemo/Models/WebApplicationApiDemoDbContext.cs
+++ WebApplicationApiDemo/Models/WebApplicationApiDemoDbContext.cs
@@ -17,16 +17,25 @@ public partial class WebApplicationApiDemoDbContext : DbContext
     {
     }
 
+    public virtual DbSet<AccessPolicy> AccessPolicies { get; set; }
+
     public virtual DbSet<Product> Products { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=WebApplicationApiDemoDb;Integrated Security=True");
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=WebApplicationApiDemoDb;Integrated Security=True;Encrypt=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<AccessPolicy>(entity =>
+        {
+            entity.HasKey(e => e.PolicyId).HasName("PK__AccessPo__2E1339A4BD3893A7");
+
+            entity.Property(e => e.PolicyId).ValueGeneratedNever();
+        });
+
         modelBuilder.Entity<Product>(entity =>
         {
             entity.Property(e => e.Id).ValueGeneratedNever();
